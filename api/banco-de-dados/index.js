04a5168d6a4d27dc92f54980881502cdbdeb4582
Mario <marioffreitas.mf@gmail.com>
2022-03-09 20:38:28 -0300

    Cria conexao com o bd

---
+++ api/banco-de-dados/index.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize')
+const config = require('config')
+
+const instancia = new Sequelize(
+    config.get('postgres.banco-de-dados'),
+    config.get('postgres.usuario'),
+    config.get('postgres.senha'),
+    {
+        host: config.get('postgres.host'),
+        dialect: 'postgres',
+        port: config.get('postgres.porta')
+    }
+
+)
+
+module.exports = instancia
