7589707786589832645150e22deb1e60ecbe8242
Mario <marioffreitas.mf@gmail.com>
2022-03-10 02:58:57 -0300

    Atualizar tabela para criacao de novas tabelas

--- api/banco-de-dados/criarTabela.js
+++ api/banco-de-dados/criarTabela.js
@@ -1,10 +1,23 @@
-// const ModeloTabela = require('../rotas/colaboradores/ModeloTabelaColaborador')
-const ModeloTabela = require('../rotas/empresas/ModeloTabelaEmpresa')
+const ModeloTabelaColaborador = require('../rotas/colaboradores/ModeloTabelaColaborador')
+const ModeloTabelaEmpresa = require('../rotas/empresas/ModeloTabelaEmpresa')
+const ModeloTabelaServico = require('../modelos/servico.js')
+const ModeloTabelaEmpresaServico = require('../modelos/empresaServico.js')
 
-ModeloTabela
-    .sync()
-    .then( () => console.log("Tabela criada com sucesso"))
+ModeloTabelaEmpresa
+    .sync({force:true})
+    .then( () => console.log("Tabela Empresa criada com sucesso"))
     .catch(console.log)
+
+ModeloTabelaServico
+    .sync({force:true})
+    .then( () => console.log("Tabela Servico criada com sucesso"))
+    .catch(console.log)
+
+ModeloTabelaEmpresaServico
+    .sync({force:true})
+    .then( () => console.log("Tabela EmpresaServico criada com sucesso"))
+    .catch(console.log)
+
 // (async () =>{
 //     const database = require('./index')
 //     const Produto = require('../rotas/colaboradores/ModeloTabelaColaborador')
